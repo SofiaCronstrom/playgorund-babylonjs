1ba83242f264f994af0b29809cf3d3b185c18c4d
SofiaCronstrom <65712065+SofiaCronstrom@users.noreply.github.com>
2021-10-21 17:24:49 +0200

    adding animation cycle on wheels, changing color on car and material on wheels

--- animation.js
+++ animation.js
@@ -11,6 +11,98 @@ const createScene = () => {
     //light
     const light = new BABYLON.HemisphericLight('light', new BABYLON.Vector3(1, 1, 0));
 
+   
+    //texture
+    const boxFur = new BABYLON.StandardMaterial('boxFur');
+    boxFur.diffuseColor = new BABYLON.Color3(0.7,0,0.2);
+    
+    //world materials
+    const car = BABYLON.MeshBuilder.CreateBox('box', {width: 0.5, height: 0.3, depth: 0.3});
+    car.material = boxFur;
+    car.rotation.x = BABYLON.Tools.ToRadians(-90);
+
+
+
+     //wheel material
+    const wheelMat = new BABYLON.StandardMaterial("wheelMat");
+    wheelMat.diffuseTexture = new BABYLON.Texture("fur.jpeg");
+    
+
+    //Wheel animation
+    const animWheel = new BABYLON.Animation('wheelAnimation', 'rotation.y', 30,
+    BABYLON.Animation.ANIMATIONTYPE_FLOAT,
+    BABYLON.Animation.ANIMATIONLOOPMODE_CYCLE);
+
+    //KEY frame animation to animate by frame number
+    const wheelKeys = [];
+
+    //At the animation key 0, the value of rotation.y is 0, animation value and frame at start
+    wheelKeys.push({
+        frame: 0,
+        value: 0
+    });
+    //At the animation key 30, (after 1 sec since animation fps = 30) the value of rotation.y is 2PI for a complete rotation, animation value and frame at end
+    wheelKeys.push({
+        frame: 30,
+        value: 2 * Math.PI
+    });
+    
+    //set the keys
+    animWheel.setKeys(wheelKeys)
+
+    
+    //right back wheel
+    const wheelRB = BABYLON.MeshBuilder.CreateCylinder('wheelRB', {diameter: 0.125, height: 0.05})
+    wheelRB.material = wheelMat;
+   wheelRB.parent = car;
+   wheelRB.position.z = -0.15;
+   wheelRB.position.x = -0.2;
+   wheelRB.position.y = 0.15;
+
+   //link the animation to the right back wheel
+   wheelRB.animations = [];
+   wheelRB.animations.push(animWheel);
+   
+   //clone right front wheel
+   const wheelRF = wheelRB.clone("wheelRF");
+   wheelRF.position.x = 0.2;
+   
+   //clone left back wheel
+   const wheelLB = wheelRB.clone("wheelLB");
+   wheelLB.position.y = -0.1 - 0.035;
+   
+   //clone right front wheel
+   const wheelLF = wheelRF.clone("wheelLF");
+   wheelLF.position.y = -0.1 - 0.035;
+
+   
+   
+   //Begin animation - object to animate, first frame, last frame and loop if true
+   scene.beginAnimation(wheelRB, 0, 30, true);
+   scene.beginAnimation(wheelRF, 0, 30, true);
+   scene.beginAnimation(wheelLB, 0, 30, true);
+   scene.beginAnimation(wheelLF, 0, 30, true);
+   
+   return scene;
+
+}
+
+
+
+const scene = createScene();//Call the createScene function
+   
+// Register a render loop to repeatedly render the scene
+engine.runRenderLoop( () => {
+ scene.render();
+});
+
+// Watch for browser/canvas resize events
+window.addEventListener('resize', () => {
+   
+   engine.resize(); 
+});
+
+
     //I NEED TO DOWNLOAD NPM EARCUT ALGORITHM
     // //base of the carshape
     // const outline = [
@@ -27,59 +119,3 @@ const createScene = () => {
     // outline.push(new BABYLON.Vector3(-0.3, 0, 0.1));
 
     // const car = BABYLON.MeshBuilder.ExtrudePolygon("car", {shape: outline, depth: 0.2});
-
-    //texture
-    const boxFur = new BABYLON.StandardMaterial('boxFur');
-    boxFur.diffuseColor = new BABYLON.Color3(1,0,0);
-
-    //world materials
-    const car = BABYLON.MeshBuilder.CreateBox('box', {width: 0.5, height: 0.3, depth: 0.3});
-    car.material = boxFur;
-
-
-    const wheelUV = [];
-    wheelUV[0] = new BABYLON.Vector4(0, 0, 1, 1);
-    wheelUV[1] = new BABYLON.Vector4(0, 0.5, 0, 0.5);
-    wheelUV[2] = new BABYLON.Vector4(0, 0, 1, 1);
-
-    const wheelMat = new BABYLON.StandardMaterial("wheelMat");
-    wheelMat.diffuseTexture = new BABYLON.Texture("https://assets.babylonjs.com/environments/wheel.png");
-    
-    const wheelRB = BABYLON.MeshBuilder.CreateCylinder('wheelRB', {diameter: 0.125, height: 0.05})
-    wheelRB.material = wheelMat;
-   wheelRB.parent = car;
-   wheelRB.position.z = -0.15;
-   wheelRB.position.x = -0.2;
-   wheelRB.position.y = 0.15;
-   
-
-   const wheelRF = wheelRB.clone("wheelRF");
-   wheelRF.position.x = 0.2;
-   
-   const wheelLB = wheelRB.clone("wheelLB");
-   wheelLB.position.y = -0.1 - 0.035;
-   
-   const wheelLF = wheelRF.clone("wheelLF");
-   wheelLF.position.y = -0.1 - 0.035;
-
-    return scene;
-
-}
-
-
-
-
-
-
-const scene = createScene();//Call the createScene function
-   
-// Register a render loop to repeatedly render the scene
-engine.runRenderLoop( () => {
- scene.render();
-});
-
-// Watch for browser/canvas resize events
-window.addEventListener('resize', () => {
-   
-   engine.resize(); 
-});
